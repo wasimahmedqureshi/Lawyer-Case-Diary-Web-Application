217f933de23264071bcfb568ba573bc4bc7c4ac8
wasimahmedqureshi <wasimahmedqureshi@gmail.com>
2026-03-18 14:00:15 +0530

    Update forms.py

--- lawyerdiary/diary/forms.py
+++ lawyerdiary/diary/forms.py
@@ -1,1 +1,12 @@
+from django import forms
+from .models import Case
 
+class CaseForm(forms.ModelForm):
+    class Meta:
+        model = Case
+        fields = '__all__'
+        widgets = {
+            'institution_date': forms.DateInput(attrs={'type': 'date'}),
+            'hearing_date': forms.DateInput(attrs={'type': 'date'}),
+            'disposal_date': forms.DateInput(attrs={'type': 'date'}),
+        }
