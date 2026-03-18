3eb960121194f4dc63aefdf5d21d312cabf318be
wasimahmedqureshi <wasimahmedqureshi@gmail.com>
2026-03-18 14:00:36 +0530

    Register Case model in Django admin

--- lawyerdiary/diary/admin.py
+++ lawyerdiary/diary/admin.py
@@ -1,1 +1,8 @@
+from django.contrib import admin
+from .models import Case
 
+@admin.register(Case)
+class CaseAdmin(admin.ModelAdmin):
+    list_display = ('case_no', 'appellant_name', 'respondent_name', 'hearing_date', 'is_disposed')
+    list_filter = ('is_disposed', 'division_name')
+    search_fields = ('case_no', 'appellant_name', 'respondent_name')
